5e697c868ec710405e1828ebf2acc09803ddceb8
Elijah <elijaherap3@gmail.com>
2020-04-19 18:34:40 -0700

    error fixes and redirection added

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl =document.querySelector("#limit-warning");
-
+var repoNameEl = document.querySelector("#repo-name");
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -16,7 +16,7 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html")
         }
     })
 };
@@ -66,6 +66,21 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
 
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
 
-getRepoIssues("facebook/react");
+        getRepoIssues(repoName);
+    }
+    else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
+
+getRepoName();
